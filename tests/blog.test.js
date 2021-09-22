5a76b21677a838ce139fcff4ca60d7cb4cfd9c98
Marsal Martínez <marsal_00@msn.com>
2021-09-22 08:10:17 -0500

    Added .travis.yml

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -1,20 +1,78 @@
+const { request } = require('express');
 const Page = require('./helpers/page');
 
 let page = new Page();
 
 beforeEach(async () => {
   page = await Page.build();
-  await page.goto('localhost:3000');
+  await page.goto('http://localhost:3000');
 });
 
 afterEach(async () => {
   await page.close();
 });
 
-test('When logged in, can see blog create form', async () => {
-  await page.login();
-  await page.click('a.btn-floating');
+describe('When logged in', async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click('a.btn-floating');
+  });
 
-  const label = await page.getContentsOf('form label');
-  expect(label).toEqual('Blog Title');
+  test('Can see blog create form', async () => {
+    const label = await page.getContentsOf('form label');
+    expect(label).toEqual('Blog Title');
+  });
+
+  describe('And using invalid inputs', async () => {
+    test('the form shows an error message', async () => {
+      await page.click('form button');
+
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
+
+  describe('And using valid inputs', async () => {
+    beforeEach(async () => {
+      await page.type('.title input', 'Título');
+      await page.type('.content input', 'Contenido del blog');
+      await page.click('form button');
+    });
+
+    test('Submitting takes user to review screen ', async () => {
+      const pageTitle = await page.getContentsOf('form h5');
+      expect(pageTitle).toEqual('Please confirm your entries');
+    });
+
+    test('Submitting then saving adds blog to index page ', async () => {
+      await page.click('button.green');
+      await page.waitFor('.card');
+
+      const title = await page.getContentsOf('.card-title');
+      const content = await page.getContentsOf('p');
+
+      expect(title).toEqual('Título');
+      expect(content).toEqual('Contenido del blog');
+    });
+  });
+});
+
+describe('When not logged in', async () => {
+  test('And try to create a blog', async () => {
+    const result = await page.post('/api/blogs', {
+      title: 'My title',
+      content: 'My content',
+    });
+
+    expect(result).toEqual({ error: 'You must log in!' });
+  });
+
+  test('And try to get the blogs', async () => {
+    const result = await page.get('/api/blogs');
+
+    expect(result).toEqual({ error: 'You must log in!' });
+  });
 });
